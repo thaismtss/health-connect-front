12c39b3fa8bc9892d899e1e85b533616f3262901
Thais Martins <thaismartins1999@gmail.com>
2023-10-08 11:13:51 -0300

    feat: add layout public pages

--- app/(public)/layout.tsx
+++ app/(public)/layout.tsx
@@ -1,8 +1,9 @@
-import './globals.css';
+import '@/app/globals.css';
 import type { Metadata } from 'next';
 import { Montserrat } from 'next/font/google';
 import Image from 'next/image';
 import IllustrationHome from '@/assets/illustration-home.svg';
+import ToastProvider from '../components/Toast';
 
 const montserrat = Montserrat({
   subsets: ['latin'],
@@ -22,8 +23,8 @@ export default function RootLayout({
   return (
     <html lang="pt">
       <body className={montserrat.className}>
-        <main className="flex min-h-screen">
-          <div className="bg-primary w-2/5 min-h-screen flex justify-center">
+        <main className="flex h-screen">
+          <div className="bg-primary w-1/2 h-screen flex justify-center">
             <div className="absolute top-10 left-10">
               <p className="text-xl font-bold text-white text-center">
                 HealthConnect
@@ -36,7 +37,9 @@ export default function RootLayout({
               width={350}
             />
           </div>
-          <div className="w-full">{children}</div>
+          <ToastProvider>
+            <div className="w-full md:w-1/2 bg-white">{children}</div>
+          </ToastProvider>
         </main>
       </body>
     </html>
